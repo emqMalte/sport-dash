e75e6ba491308ed961833c71c0aa29f02ccc86f1
emqMalte <7808912+emqMalte@users.noreply.github.com>
2024-04-12 12:32:31 +0200

    loading and error display

--- src/components/Schedule.tsx
+++ src/components/Schedule.tsx
@@ -1,9 +1,11 @@
 import { useQuery } from "@tanstack/react-query";
 import { Schedule as MlbSchedule } from "../types/mlb/Schedule";
 import { ScheduleGameCard } from "./ScheduleGameCard";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSpinner } from "@fortawesome/free-solid-svg-icons";
 
 export const Schedule = () => {
-  const { isPending, error, data } = useQuery<MlbSchedule>({
+  const { isLoading, error, data } = useQuery<MlbSchedule>({
     queryKey: ["schedule"],
     queryFn: () =>
       fetch(
@@ -18,6 +20,22 @@ export const Schedule = () => {
     <div>
       <h1 className="my-4 text-center text-6xl font-bold">MLB Schedule</h1>
 
+      {isLoading && (
+        <div className="my-16 text-center text-2xl font-bold">
+          <FontAwesomeIcon
+            icon={faSpinner}
+            className="mx-auto my-2 block animate-spin text-6xl"
+          />
+          Loading...
+        </div>
+      )}
+
+      {error && (
+        <div className="rounded border bg-red-800 p-4 text-center text-2xl text-white drop-shadow-xl">
+          <span className="font-extrabold">Error</span>: {error.message}
+        </div>
+      )}
+
       {data?.dates.map((date) => (
         <div key={date.date.toString()}>
           <h2 className="my-3 text-center text-2xl font-bold">
@@ -27,10 +45,6 @@ export const Schedule = () => {
             All times are in your local timezone (
             {Intl.DateTimeFormat().resolvedOptions().timeZone})
           </p>
-
-          {isPending && <p>Loading...</p>}
-
-          {error && <p>Error: {error.message}</p>}
 
           <div className="my-8 flex flex-wrap justify-center">
             {date.games.map((game) => (
