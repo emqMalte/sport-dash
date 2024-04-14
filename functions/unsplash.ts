75ed1248826fcb7c1dea71054535e2bc18828697
emqMalte <7808912+emqMalte@users.noreply.github.com>
2024-04-14 15:26:21 +0200

    can i return the async fetch as is?

--- functions/unsplash.ts
+++ functions/unsplash.ts
@@ -1,3 +1,22 @@
-export function onRequest() {
-  return new Response("Hello, world!");
+interface Env {
+  UNSPLASH_APPLICATION_ID: string;
+  UNSPLASH_ACCESS_KEY: string;
+  UNSPLASH_SECRET_KEY: string;
 }
+
+export const onRequest: PagesFunction<Env> = (context) => {
+  const url = new URL("https://api.unsplash.com/photos/random");
+
+  const urlParams = new URLSearchParams();
+  urlParams.append("query", "ballpark major league baseball MLB");
+  urlParams.append("orientation", "landscape");
+  urlParams.append("count", "1");
+
+  url.search = urlParams.toString();
+
+  return fetch(url, {
+    headers: {
+      Authorization: `Client-ID ${context.env.UNSPLASH_ACCESS_KEY}`,
+    },
+  });
+};
