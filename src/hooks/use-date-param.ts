30e81abc126cd499ea2642554320896af9ac46cc
BobTheBuilder796 <191618178+BobTheBuilder796@users.noreply.github.com>
2025-03-27 18:08:03 +0100

    feat: enhance useDateParam hook to handle current date and nullify date for today

--- src/hooks/use-date-param.ts
+++ src/hooks/use-date-param.ts
@@ -1,10 +1,16 @@
 import { parseAsIsoDate, useQueryState } from "nuqs";
 
 export function useDateParam() {
-  const [date, setDate] = useQueryState(
+  const now = new Date();
+  const [date, setQueryDate] = useQueryState(
     "date",
-    parseAsIsoDate.withDefault(new Date()),
+    parseAsIsoDate.withDefault(now),
   );
+
+  function setDate(date: Date) {
+    const newDate = date.toDateString() === now.toDateString() ? null : date;
+    return setQueryDate(newDate);
+  }
 
   return [date, setDate] as const;
 }
