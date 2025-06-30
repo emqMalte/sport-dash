e96f31bc76b8c1f6769511e7eba343ab1d07a7f9
emqMalte <7808912+emqMalte@users.noreply.github.com>
2025-06-30 16:27:47 +0200

    chore: add unplugin-inject-preload dependency and configure it in vite.config.ts for improved asset preloading

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 // import react from "@vitejs/plugin-react-swc";
 import react from "@vitejs/plugin-react";
 import { analyzer } from "vite-bundle-analyzer";
+import UnpluginInjectPreload from "unplugin-inject-preload/vite";
 
 const ReactCompilerConfig = {
   /* ... */
@@ -14,6 +15,13 @@ export default defineConfig({
       babel: {
         plugins: [["babel-plugin-react-compiler", ReactCompilerConfig]],
       },
+    }),
+    UnpluginInjectPreload({
+      files: [
+        {
+          outputMatch: /index-[a-zA-Z0-9-_]*.(css|js)$/,
+        },
+      ],
     }),
     analyzer({
       analyzerMode: "static",
